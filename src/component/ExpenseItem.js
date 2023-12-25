c6011b8a701060573d3e9350f0e5f88042058868
kartikaysingh12 <146762244+kartikaysingh12@users.noreply.github.com>
2023-12-25 22:26:14 +0530

    reactapp

---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,17 @@
+function ExpenseItem(){
+    return(<div>
+        <div><h2> Expense Items</h2>
+        </div>
+        <div>CHOOSE EXPENSES</div>
+    <select>
+        <option>Food
+        </option>
+        <option>Electricity
+
+        </option>
+        <option>housekeeper</option>
+    </select>
+    </div> 
+    );
+}
+export default ExpenseItem;
