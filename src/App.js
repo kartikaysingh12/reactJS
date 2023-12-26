0f22629dbfcfa99871acfa4093104c8d8c69b979
kartikaysingh12 <146762244+kartikaysingh12@users.noreply.github.com>
2023-12-26 18:00:17 +0530

    withStyling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './component/ExpenseItem';
 function App() {
+  const expenses=[
+    {
+      id :"e1",title:"Car insurance", amount: 800, date:new Date(2022, 2 ,11)
+    },
+    {
+      id :"e2",title:"Electricity", amount: 1100, date:new Date(2022, 4 ,13)
+    },
+    {
+      id :"e3",title:"Home Renovation", amount: 1500, date:new Date(2022, 3 ,15)
+    }
+
+  ]
   return (
     <div className="App">
       <header className="App-header">
@@ -9,7 +21,21 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <ExpenseItem></ExpenseItem>
+        <ExpenseItem 
+        title={expenses[0].title} 
+        amount={expenses[0].amount}
+        date={expenses[0].date}>
+        </ExpenseItem>
+        <ExpenseItem 
+        title={expenses[1].title} 
+        amount={expenses[1].amount}
+        date={expenses[1].date}>
+        </ExpenseItem>
+        <ExpenseItem 
+        title={expenses[2].title} 
+        amount={expenses[2].amount}
+        date={expenses[2].date}>
+        </ExpenseItem>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,17 +1,19 @@
-function ExpenseItem(){
-    return(<div>
-        <div><h2> Expense Items</h2>
+import './ExpenseItem.css';
+function ExpenseItem(props){
+    // const expenseDate=new Date(2022, 2, 27);
+    // const expenseTitle="Car Insurance";
+    // const expenseAmount=295;
+    return(<div className="expense-item">
+        <div >{props.date.toISOString()}</div>
+        <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">Rs{props.amount}</div>
         </div>
-        <div>CHOOSE EXPENSES</div>
-    <select>
-        <option>Food
-        </option>
-        <option>Electricity
-
-        </option>
-        <option>housekeeper</option>
-    </select>
+        <div>{"Location is " + props.title}</div>
     </div> 
+    
     );
+    
+
 }
 export default ExpenseItem;
