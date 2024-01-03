6427dd77e80ae1636516b936d1eb415225721cba
kartikaysingh12 <146762244+kartikaysingh12@users.noreply.github.com>
2024-01-03 13:38:35 +0530

    IntroduceStateInsideNewComponent

--- src/component/ExpenseForm/NewExpenseForm.js
+++ src/component/ExpenseForm/NewExpenseForm.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NewExpesneForm.css';
 const NewExpenseForm=()=>{
-    const handleEvent=(event)=>{
-        console.log(event.target.value)
+const[newTitle,enteredTitle]=useState('' );
+const[newAmount,enteredAmount]=useState('' );
+const[newDate,enteredDate]=useState('' );
+    const handleEventTitle=(event)=>{
+        enteredTitle(event.target.value)
+    }
+    const handleEventAmount=(event)=>{
+        enteredAmount(event.target.value)
+    }
+    const handleEventDate=(event)=>{
+        enteredDate(event.target.value)
     }
     return <form>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label className="new-expense__control label">Title</label>
-                <input className="new-expense__control input" type='text' placeholder='Enter title' onClickCapture={handleEvent}/>
+                <input className="new-expense__control input" type='text' placeholder='Enter title' onClickCapture={handleEventTitle}/>
 
             </div>
             <div className='new-expense__control'>
                 <label className="new-expense__control label">Amount</label>
-                <input className="new-expense__control input" type='number' placeholder='Enter Number' onClickCapture={handleEvent}/>
+                <input className="new-expense__control input" type='number' placeholder='Enter Number' onClickCapture={handleEventAmount}/>
 
             </div>
             <div className='new-expense__control'>
                 <label className="new-expense__control label">Date</label>
-                <input className="new-expense__control input" type='date' placeholder='Enter Date' min='2021-01-01' max="2024-12-31" onClickCapture={handleEvent}/>
+                <input className="new-expense__control input" type='date' placeholder='Enter Date' min='2021-01-01' max="2024-12-31" onClickCapture={handleEventDate}/>
 
             </div>
         </div>
